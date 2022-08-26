32c9a3e814cda4d3d25fabb62fa8d4a4536fd679
ShwStone <90076553+ShwStone@users.noreply.github.com>
2022-08-26 09:10:11 +0000

    upd

--- 20220824/Problem/std/love/baoli.cpp
+++ 20220824/Problem/std/love/baoli.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define FILE_IO
+// #define FILE_IO
 namespace io {
 	#ifdef FILE_IO
 	#define FREAD
@@ -41,8 +41,53 @@ namespace io {
 };
 using namespace io;
 
+const int maxn = 1e3 + 5;
+
+struct frac {
+    long long a, b;
+    void fix() { if (a == 0) return void(b = 1); long long d = __gcd(a, b); a /= d; b /= d; }
+    frac() { a = 0, b = 1; }
+    frac(long long x) { a = x, b = 1; }
+    frac(long long x, long long y) { a = x, b = y; }
+    frac operator *= (frac x) { a *= x.a, b *= x.b; fix(); return *this; }
+    bool operator < (frac x) const { return a * x.b < x.a * b; }
+};
+
+frac operator * (frac a, frac b) { return a *= b; }
+
+struct node {
+    int v, op;
+    frac x;
+};
+
+vector<node> graph[maxn];
+int n, m;
+frac ans;
+
+void dfs(int u, int bro, frac dis) {
+	if (u == n) ans = max(ans, dis);
+	for (node e : graph[u]) {
+		if (e.op) {
+			if (bro) dfs(e.v, bro - 1, dis);
+			else dfs(e.v, bro, dis * e.x);
+		}
+		else dfs(e.v, bro + 1, dis);
+	}
+}
+
 int main() {
-    freopen(".in", "r", stdin);
-    freopen(".out", "w", stdout);
+    // freopen(".in", "r", stdin);
+    // freopen(".out", "w", stdout);
+	read(n); read(m);
+	for (int i = 1; i <= n; i++) {
+		int u; read(u);
+		node x; read(x.v); read(x.op);
+		if (x.op) read(x.x.a), read(x.x.b);
+		graph[u].push_back(x);
+	}
+	dfs(1, 0, frac(1));
+	if (!ans.a) putchar('B'), putchar('E'), putchar('\n');
+    else if (ans.a == ans.b) putchar('H'), putchar('E'), putchar('\n');
+    else write(ans.a, '/'), write(ans.b);
     return 0;
 }
