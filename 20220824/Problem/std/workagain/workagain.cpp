913d02ac82d0ac7a7179d96e70ce414f9cb58bdf
ShwStone <90076553+ShwStone@users.noreply.github.com>
2022-08-26 03:01:19 +0000

    upd

--- 20220824/Problem/std/workagain/workagain.cpp
+++ 20220824/Problem/std/workagain/workagain.cpp
@@ -52,14 +52,17 @@ int main() {
     // freopen(".out", "w", stdout);
 	read(n); read(m);
 	for (int i = 1; i <= m; i++) read(t[i]);
-	for (int i = 1; i <= n; i++) scanf("%lf", v + i);
+	for (int i = 1; i <= n; i++) {
+		scanf("%lf", v + i);
+		v[i] += v[i - 1];
+	}
 	for (int i = 1; i <= n; i++) read(w[i]);
 	for (int i = 1; i <= m; i++) dp[0][i] = 1e9;
 	for (int i = 1; i <= n; i++) {
 		for (int j = 0; j <= m; j++) {
 			dp[i][j] = dp[i - 1][j];
 			for (int k = 1; k <= min(j, w[i]); k++) {
-				dp[i][j] = min(dp[i][j], dp[i - 1][j - k] + v[i] * double(t[i]));
+				dp[i][j] = min(dp[i][j], dp[i - 1][j - k] + (v[j] - v[j - k]) * double(t[i]));
 			}
 			printf("%.2lf%c", dp[i][j], " \n"[j == m]);
 		}
